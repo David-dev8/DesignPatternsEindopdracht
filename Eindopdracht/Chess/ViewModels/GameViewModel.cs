69bfb0a5097264a6450f322505822dbf8ffe20c6
David-dev8 <72741163+David-dev8@users.noreply.github.com>
2023-03-22 09:25:03 +0100

    Class files
    Co-Authored-By: Nathan511 <72741392+Nathan511@users.noreply.github.com>
    Co-Authored-By: Tim <72740253+Tim070@users.noreply.github.com>

---
+++ Eindopdracht/Chess/ViewModels/GameViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Chess.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess.ViewModels
+{
+    public class GameViewModel : BaseViewModel
+    {
+        public GameViewModel(NavigationService navigationService) : base(navigationService)
+        {
+        }
+    }
+}
